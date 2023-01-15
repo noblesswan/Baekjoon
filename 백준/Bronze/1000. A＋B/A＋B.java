1332ddd6091f86b0c396ddd674dd8799296a054d
noblesswan <81548882+noblesswan@users.noreply.github.com>
2023-01-15 13:03:47 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		int firstNum = scanner.nextInt();
+		int secondNum = scanner.nextInt();
+		
+		System.out.println(firstNum + secondNum);
+		
+	}
+}
