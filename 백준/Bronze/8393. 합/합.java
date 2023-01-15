cbb2c84d5a6f393a22430e1735af1f5a99f646e5
noblesswan <81548882+noblesswan@users.noreply.github.com>
2023-01-15 14:02:46 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		int sum=0;
+		
+		for (int i = 1; i < number+1; i++)
+		{
+			sum += i;
+		}
+		System.out.println(sum);
+		
+	}
+}
